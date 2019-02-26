0143b6fae9fc2a0d9ad6e66cd8ea567cf53e2561
PengjuYan <yanpengju@gmail.com>
2019-02-26 13:25:29 +0800

    Format code

--- extension/popup.js
+++ extension/popup.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#config').addEventListener('click', function() {
+  document.querySelector('#config').addEventListener('click', function () {
     window.open(chrome.runtime.getURL("options.html"));
   });
-  
-  document.querySelector('#about').addEventListener('click', function() {
+
+  document.querySelector('#about').addEventListener('click', function () {
     window.open("https://github.com/buggyj/savetiddlers");
   });
 
